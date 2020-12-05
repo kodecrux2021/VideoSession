c08133bd0efa5d57b7e486f52941bb79daf4762f
= <=>
2020-12-05 10:12:27 +0530

    trainers page layout done

--- frontend/src/Container/Trainers/TrainersCard.js
+++ frontend/src/Container/Trainers/TrainersCard.js
@@ -27,7 +27,7 @@ const styles = theme => ({
                 <div className="d-flex flex-column trainers__card__details "> 
                     <div >
                         <span style={{color: "#5bc0de", marginRight: "20px"}}><strong>{props.name}</strong></span> 
-                        {/* {props.online ? <span style={{color:"rgb(85, 243, 124)"}}>
+                        {props.online ? <span style={{color:"rgb(85, 243, 124)"}}>
                           <Badge  classes={{ badge: classes.customBadge }}
                             overlap="circle"
                             badgeContent=""
@@ -38,12 +38,12 @@ const styles = theme => ({
                             overlap="circle"
                             badgeContent=""
                             variant="dot"
-                            ></Badge> Offline</span>} */}
+                            ></Badge> Offline</span>}
                     </div>
                     <div >
                         <span style={{marginRight: "20px"}}><strong>US ${props.rate}</strong> / {props.time} mins</span>
                         <span style={{marginRight: "12px"}}> <Star style={{paddingBottom:"5px"}} /> <strong>{props.rating}</strong> ({props.reviews} reviews) </span>
-                        <StarsIcon />
+                        {props.badge?<StarsIcon />:null}   
                     </div>
                     <span>{props.details}</span>
                 </div>

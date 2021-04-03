1e4623a4680114d6922d89625a403847cd3aa83e
unknown <tanyas1058@gmail.com>
2021-04-03 12:32:51 +0530

    chat bugs fixed

--- frontend/src/Container/Chat/Chat.js
+++ frontend/src/Container/Chat/Chat.js
@@ -180,9 +180,9 @@ export default function Chat(props) {
             <div className='chat__footer'>
                 <form>
                     <input placeholder='Type a message' value={props.message} onChange={(e)=>props.handleData('message', e.target.value)} type="text" />
-                    {/* <button type='submit' onClick={(e)=>props.sendMessage(e)}>Send a message</button> */}
+                    <button type='submit' onClick={(e)=>props.sendMessage(e)}>Send a message</button>
                 </form>
-                <IconButton type = 'submit' onSubmit={(e)=>props.sendMessage(e)}><SendIcon className="svg_icons"/></IconButton>
+                <IconButton  onClick={(e)=>props.sendMessage(e)}><SendIcon className="svg_icons"/></IconButton>
                 {/* <IconButton><PictureAsPdfIcon className="svg_icons"/></IconButton>                           */}
             </div>
 

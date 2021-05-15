1b36f8fd60c2574241f468804cdad0f3635a4ee7
abhishekranaji <abhishek.abhirana.rana2@gmail.com>
2021-05-16 01:08:30 +0530

    Investor page ui fix

--- src/Container/Investor/Investor.js
+++ src/Container/Investor/Investor.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import './Investor.css'
-import { Avatar  } from '@material-ui/core';
+import { Avatar, Input, TextField  } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Select from 'react-select';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
@@ -38,129 +38,142 @@ export default function Investor(props) {
         setVisible(false)
     }
 
+    const customStyles = {
+        control: (base, state) => ({
+          ...base,
+          padding: 5,
+          borderColor : '#3743B1',
+          boxShadow: "none",
+          color : '#3743B1',
+          // You can also use state.isFocused to conditionally style based on the focus state
+        }),
+        menuPortal: base => ({ ...base, zIndex: 9999 })
+      };
+
     return (
         <div className='investor' > 
-        <div style={{maxWidth: '900px', marginTop: '30px'}} >
-        <div className='investor__head'>
-                <div className='investor__head__title'>
-                <Avatar src={props.pic} className={classes.large} />
-                <span className='invester__heading' >Create a freelance job for {props?.investor__name}</span>
+            <div style={{maxWidth: '1000px', marginTop: '30px'}} className="investor_Card" >
+            <div className='investor__head'>
+                    <div className='investor__head__title'>
+                    <Avatar src={props.pic} className={classes.large} />
+                    <span className='invester__heading' style={{fontSize:24}} >Create a freelance job for {props?.investor__name}</span>
+                    </div>
+                    <div className='investor__head__detail'>
+                    Congratulations on finding a mentor for your project! To make sure
+                    that you and {props?.investor__name} are on the same page, please discuss project 
+                    deliverables and deadline , then fill in the information below to create the freelance job.
+                    </div>
                 </div>
-                <div className='investor__head__detail'>
-                 Congratulations on finding a mentor for your project! To make sure
-                 that you and {props?.investor__name} are on the same page, please discuss project 
-                 deliverables and deadline , then fill in the information below to create the freelance job.
-                </div>
-            </div>
-            <div className='investor__body'>
-            <div className='investor__card'>
-                <div style={{display: 'flex'}} >
-                <div className='round__icon' >
-                    1
-                </div>
-               <p className='invester__heading' >Which request is the mentor helping you with ?</p>
-                </div>
-                <span className='investor__head__detail' >This request you choose will be closed and no longer shown to mentors.</span>
-               
-                <div style={{ margin:'5px 0'}} >
-                <Select
+                <div className='investor__body'>
+                <div className='investor__card'>
+                    <div style={{display: 'flex'}} >
+                    <div className='round__icon' >
+                        1
+                    </div>
+                <p className='invester__heading' >Which request is the mentor helping you with ?</p>
+                    </div>
+                    {/* <span className='investor__head__detail' >This request you choose will be closed and no longer shown to mentors.</span> */}
+                
+                    {/* <div style={{ margin:'5px 0'}} > */}
+                    <Select
 
-                className="basic-multi-select"
-                classNamePrefix="select"
-                onChange={props.handleSelect}
-                getOptionLabel={option =>
-                  `${option.name}`
-                }
-                getOptionValue={option => `${option.id}`}
-                name="colors"
-                options={props.sampleData}
-                isSearchable={true}        
-                placeholder={'Search for anything'}
-              />
-                </div>
+                    className="basic-multi-select"
+                    classNamePrefix="select"
+                    onChange={props.handleSelect}
+                    getOptionLabel={option =>
+                    `${option.name}`
+                    }
+                    styles={customStyles}
+                    getOptionValue={option => `${option.id}`}
+                    name="colors"
+                    options={props.sampleData}
+                    isSearchable={true}        
+                    placeholder={'TECHNOLOGY'}
+                />
+                    {/* </div> */}
 
-            </div>
-
-            <div className='investor__card'>
-                <div style={{display: 'flex'}} >
-                <div className='round__icon' >
-                    2
-                </div>
-               <p className='invester__heading' >Project details</p>
-                </div>
-                <span className='investor__head__detail' >Project title *</span>
-               
-                <input style={{backgroundColor:'whitesmoke'}} type="text" className="form__control"  placeholder="Enter Project Title" onChange={(e) => props.handleChange('title',e.target.value)} />
-                   <div style={{marginTop:'25px'}} ></div>
-                <span  className='investor__head__detail' >Deliverables *</span>
-               
-               <input style={{backgroundColor:'whitesmoke'}} type="text" className="form__control"  placeholder="Enter Deliverables"  onChange={(e) => props.handleChange('deliverables',e.target.value)}/>
-            </div>
-
-            <div className='investor__card'>
-                <div style={{display: 'flex'}} >
-                <div className='round__icon' >
-                    3
-                </div>
-               <p className='invester__heading' >Budget and deadline</p>
                 </div>
 
-                <span className='investor__head__detail' >Budget* (INR) </span>
-                <div className = 'budget' >
-                {/* <AttachMoneyIcon/> */}
+                <div className='investor__card'>
+                    <div style={{display: 'flex'}} >
+                    <div className='round__icon' >
+                        2
+                    </div>
+                <p className='invester__heading' >Project details</p>
+                    </div>
+                    {/* <span className='investor__head__detail' ></span> */}
+                    
+                    <TextField variant='outlined'  type="text" className="form__control"  label="PROJECT TITLE*" onChange={(e) => props.handleChange('title',e.target.value)} />
+                    <div style={{marginTop:'25px'}} ></div>
+                    {/* <span  className='investor__head__detail' ></span> */}
+                
+                <TextField variant='outlined'  type="text" className="form__control"  label="DELIVERABLES *"  onChange={(e) => props.handleChange('deliverables',e.target.value)}/>
+                </div>
 
-                <div style={{ margin:'5px 20px 5px 0px'}} >
-                <input style={{backgroundColor:'whitesmoke'}} type="text" className="form__control"  placeholder="Enter Budget" onChange={(e) => props.handleChange('budget',e.target.value)}  />
-                </div>
-                {/* <span className='investor__head__detail' > Maximum  INR 2000 </span> */}
-                <div style={{marginLeft:'5px'}} >
-                <HelpOutlineIcon/>
-                </div>
+                <div className='investor__card'>
+                    <div style={{display: 'flex'}} >
+                    <div className='round__icon' >
+                        3
+                    </div>
+                <p className='invester__heading' >Budget and deadline</p>
+                    </div>
+
+                    {/* <span className='investor__head__detail' >Budget* (INR) </span> */}
+                    <div className = 'budget' >
+                    {/* <AttachMoneyIcon/> */}
+
+                    <div style={{ margin:'5px 20px 5px 0px'}} >
+                    <TextField variant='outlined' type="text" className="form__control"  label="BUDGET* (INR)" onChange={(e) => props.handleChange('budget',e.target.value)}  />
+                    </div>
+                    {/* <span className='investor__head__detail' > Maximum  INR 2000 </span> */}
+                    <div style={{marginLeft:'5px'}} >
+                    <HelpOutlineIcon/>
+                    </div>
+                    
+                    </div>
+                    
+                    <div style={{marginTop:'25px'}} ></div>
+                    <span  className='investor__head__detail' style={{color:'#3743B1'}} >Deadlines*</span>
+                    <div  style={{display:'flex', alignItems:'center'}} >
+                    <span className='investor__head__detail' style={{color:'#3743B1'}} > Before the end of  </span>
+                    <div style={{margin: '5px 20px'}} >
+                    <DatePicker style={{borderColor:'#3743B1', borderRadius:5}} onChange={props.onChange} size='large' />
+                    </div>
+                    <HelpOutlineIcon/>
+                    </div>
+
                 
                 </div>
+
+                <div className='investor__card'>
+                    <div style={{display: 'flex'}} >
+                    <div className='round__icon' >
+                        4
+                    </div>
+                <p className='invester__heading' >Additional information</p>
+                    </div>
+                    {/* <span className='investor__head__detail' >Project background, links, and addtional terms </span> */}
+                    
+                    <TextField variant='outlined' multiline rows={4}  className="employ__reg__textarea"  label='PROJECT BACKGROUND, LINKS, AND ADDITIONAL TERMS ' onChange={(e) => props.handleChange('info',e.target.value)}>
+                    </TextField>
+        
                 
-                <div style={{marginTop:'25px'}} ></div>
-                <span  className='investor__head__detail' >Deadlines*</span>
-                <div  style={{display:'flex', alignItems:'center', color:'gray'}} >
-                <span className='investor__head__detail' > Before the end of  </span>
-                <div style={{margin: '5px 20px'}} >
-                <DatePicker onChange={props.onChange} size='large' />
-                </div>
-                <HelpOutlineIcon/>
+
                 </div>
 
-               
-            </div>
+                <span className='investor__head__detail' style={{fontSize:12, color:'#3742B1'}} > * The details of the freelance job will be final after it has been created. Please make sure all the information above is correct. </span>
 
-            <div className='investor__card'>
-                <div style={{display: 'flex'}} >
-                <div className='round__icon' >
-                    4
                 </div>
-               <p className='invester__heading' >Additional information</p>
+                <div className="form-actions">
+                <button className="hire_btn" onClick = {() => hire()}>HIRE</button>
+                            </div>
+                            <Modal title="Are you sure you want to hire?" visible={isModalVisible}  footer={null} >
+                <div className='reg__modal__button' >
+                <button onClick={ () => send() } className="send_req" >Send Request</button>
+                <button onClick={()=>cancel()} className="cancel_btn" >Cancel</button>
                 </div>
-                <span className='investor__head__detail' >Project background, links, and addtional terms </span>
-                
-                <textarea style={{backgroundColor:'whitesmoke'}} className="employ__reg__textarea"  placeholder='Enter Information' onChange={(e) => props.handleChange('info',e.target.value)}>
-                </textarea>
-      
-               
-
+            </Modal>
             </div>
-
-            <span className='investor__head__detail' > * The details of the freelance job will be final after it has been created. Please make sure all the information above is correct. </span>
-
-            </div>
-            <div className="form-actions">
-            <button style = {{backgroundColor: ' rgb(15, 15, 151)'}} onClick = {() => hire()}>HIRE</button>
-                        </div>
-                        <Modal title="Are you sure you want to hire?" visible={isModalVisible}  footer={null} >
-            <div className='reg__modal__button' >
-            <button onClick={ () => send() } style={{  backgroundColor:' #5964c9'}} type="primary" size='large' >Send Request</button>
-            <button onClick={()=>cancel()} style={{  backgroundColor:' #76d2fd'}} type="primary" size='large' >Cancel</button>
-            </div>
-           </Modal>
-        </div>
         
         </div>
     )
